6a5a108c0fa9430df5f28a2ffc457c0aa1246a55
Fernand <fernando.vgarcia@hotmail.com>
2020-04-15 22:53:03 -0300

    New incident page created.

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -11,7 +11,8 @@ function Register(){
         <div className="register-container">
             <div className="content">
                 <section>
-                    <img src={logoImg} alt="Be The Hero" />                    <h1>Cadastro</h1>
+                    <img src={logoImg} alt="Be The Hero" />
+                    <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#e02041" />
