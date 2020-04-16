c8e737728a323d78ec30676e8b9a803577df9c59
Fernand <fernando.vgarcia@hotmail.com>
2020-04-15 21:36:54 -0300

    Frontend web initialized. Logon page created.

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+import './global.css';
+
+import Logon from './pages/Logon';
+
+function App() {
+
+    return (
+        <div>
+            <Logon />
+        </div>
+    );
+}
+
+export default App;
